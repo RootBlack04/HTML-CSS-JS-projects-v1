34958f1282c48e76ed7e972f6fdc5635de65ba2b
Abdallah Torkamani <abdellahturkmani@gmail.com>
2024-05-19 14:11:36 +0100

    "push projects 5/19/2024"

---
+++ Loading Bar/script.js
@@ -0,0 +1,15 @@
+const conterEl = document.querySelector(".counter");
+const barEl = document.querySelector(".loading-bar-front");
+
+let idx = 0;
+
+updateNum();
+function updateNum() {
+  conterEl.innerText = idx + "%";
+
+  barEl.style.width = idx + "%";
+  idx++;
+  if (idx < 101) {
+    setTimeout(updateNum, 100);
+  }
+}
